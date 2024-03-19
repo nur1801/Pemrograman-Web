4cebc71df670fb6f86e755d00fce53b2af20b6b7
nur1801 <nurhid180701@gmail.com>
2024-03-19 12:46:13 +0700

    Update_Menggabungkan HTML dan PHP

---
+++ Pertemuan6/mergeHtmlPhp.php
@@ -0,0 +1,13 @@
+<!-- MENGGABUNUGKAN HTML DAN PHP -->
+<html>
+  <head>
+    <title>Cara 01</title>
+  </head>
+  <body>
+    <p>
+      Tanggal Hari ini :
+      <?php echo date("d M Y")?>
+      <!-- lebih mudah menggunakan cara 01 karena program lebih mudah diingat -->
+    </p>
+  </body>
+</html>
