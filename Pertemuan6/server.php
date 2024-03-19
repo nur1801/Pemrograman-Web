b82ee56bb8157711d9b7e3f0184bf475c5626dc3
nur1801 <nurhid180701@gmail.com>
2024-03-19 12:59:34 +0700

    Update_Variable Server

---
+++ Pertemuan6/server.php
@@ -0,0 +1,21 @@
+<?php
+// soal no 21
+echo json_encode($_SERVER);
+
+// soal no 22
+// echo $_SERVER['PHP_SELF'];
+// echo "<br><br>";
+// echo $_SERVER['SERVER_NAME'];
+// echo "<br><br>";
+// echo $_SERVER['HTTP_HOST'];
+// echo "<br><br>";
+// if (isset($_SERVER['HTTP_REFERER'])) {
+//     echo $_SERVER['HTTP_REFERER'];
+// } else {
+//     echo "HTTP_REFERER tidak ada. <br>";
+// }
+// echo $_SERVER['HTTP_USER_AGENT'];
+// echo "<br><br>";
+// echo $_SERVER['SCRIPT_NAME'];
+// echo "<br>";
+?>
