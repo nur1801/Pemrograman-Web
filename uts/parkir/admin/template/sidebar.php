4a85d518b8bc4abdc8482d7de197782a6e896113
nur1801 <nurhid180701@gmail.com>
2024-04-22 09:35:38 +0700

    UTS - Sistem Parkir Berbasis Website

--- uts/parkir/admin/template/sidebar.php
+++ uts/parkir/admin/template/sidebar.php
@@ -7,25 +7,25 @@
     <ul class="nav">
         <li>
             <a class="nav-link" href="index.php">
-                <i class="nc-icon nc-chart-pie-35"></i>
+                
                 <p>Dashboard</p>
             </a>
         </li>
         <li>
             <a class="nav-link" href="kendaraan.php">
-                <i class="nc-icon nc-bus-front-12"></i>
+                
                 <p>Data Kendaraan</p>
             </a>
         </li>
         <li>
             <a class="nav-link" href="jenis_kendaraan.php">
-                <i class="nc-icon nc-delivery-fast"></i>
+                
                 <p>Jenis Kendaraan</p>
             </a>
         </li>
         <li>
             <a class="nav-link" href="../index.php">
-                <i class="nc-icon nc-bank"></i>
+                
                 <p>Homepage</p>
             </a>
         </li>
